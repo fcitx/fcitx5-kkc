4392d6a3b53f85768cacadd9a990418ed6ad2a59
luren <byljcron@gmail.com>
2017-12-15 14:20:37 -0600

    init comit

--- src/kkc.cpp
+++ src/kkc.cpp
@@ -1,1 +1,43 @@
+#include "kkc.h"
+namespace fcitx {
+class KKCState : public InputContextProperty {
+public:
+    KKCState(KKC *parent)
+        : parent_(parent),
+          context_(kkc_context_new(parent->model()), &g_object_unref) {}
 
+    KKC *parent_;
+    std : unique_ptr<KkcContext, decltype(&g_object_unref)> context_;
+};
+KKC::KKC(Instance *instance)
+    : instance_(instance),
+      factory_([this](InputContext &) { return new KKCState(this); }),
+      model_(nullptr, &g_object_unref) {
+#if !GLIB_CHECK_VERSION(2, 36, 0)
+    g_type_init();
+#endif
+    kkc_init();
+
+    model_.reset(kkc_language_model_load("sorted3", NULL));
+    loadDictionary();
+    loadRule();
+
+    reloadConfig();
+    instance_->inputContextManager().registerProperty("kkcState", &factory_);
+}
+
+KKC ::~KKC() {}
+void KKC::activate(const InputMethodEntry &entry, InputContextEvent &event) {}
+void KKC::keyEvent(const InputMethodEntry &entry, KeyEvent &keyEvent) {}
+void KKC::reloadConfig() {}
+void KKC::reset(const InputMethodEntry &entry, InputContextEvent &event) {}
+void KKC::save() {}
+
+void KKC::updateUI(InputContext *inputContext) {}
+
+std::string KKC::subMode(const InputMethodEntry &, InputContext &) {}
+
+KKCState *KKC::state(InputContext *ic) {}
+
+} // namespace fcitx
+FCITX_ADDON_FACTORY(fcitx::KKCFactory);
--- src/kkc.h
+++ src/kkc.h
@@ -1,22 +1,73 @@
-/***************************************************************************
- *   Copyright (C) 2013~2013 by CSSlayer                                   *
- *   wengxt@gmail.com                                                      *
- *                                                                         *
- *  This program is free software: you can redistribute it and/or modify   *
- *  it under the terms of the GNU General Public License as published by   *
- *  the Free Software Foundation, either version 3 of the License, or      *
- *  (at your option) any later version.                                    *
- *                                                                         *
- *  This program is distributed in the hope that it will be useful,        *
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of         *
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          *
- *  GNU General Public License for more details.                           *
- *                                                                         *
- *  You should have received a copy of the GNU General Public License      *
- *  along with this program.  If not, see <http://www.gnu.org/licenses/>.  *
- *                                                                         *
- ***************************************************************************/
+/*
+ * Copyright (C) 2017~2017 by CSSlayer
+ * wengxt@gmail.com
+ *
+ * This library is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of the
+ * License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; see the file COPYING. If not,
+ * see <http://www.gnu.org/licenses/>.
+ */
+#ifndef _FCITX_KKC_H_
+#define _FCITX_KKC_H_
 
-#ifndef _FCITX_Kkc_H_
-#define _FCITX_Kkc_H_
-#endif
+#include <fcitx-config/configuration.h>
+#include <fcitx/addonfactory.h>
+#include <fcitx/addonmanager.h>
+#include <fcitx/inputcontextproperty.h>
+#include <fcitx/inputmethodengine.h>
+#include <fcitx/instance.h>
+#include <libkkc/libkkc.h>
+#include <memory>
+
+namespace fcitx {
+
+class KKCState;
+
+class KKC final : public InputMethodEngine {
+public:
+    KKC(Instance *instance);
+    ~KKC();
+    Instance *instance() { return instance_; }
+    void activate(const InputMethodEntry &entry,
+                  InputContextEvent &event) override;
+    void keyEvent(const InputMethodEntry &entry, KeyEvent &keyEvent) override;
+    void reloadConfig() override;
+    void reset(const InputMethodEntry &entry,
+               InputContextEvent &event) override;
+    void save() override;
+    auto &factory() { return factory_; }
+    auto model() { return model_.get(); }
+
+    void updateUI(InputContext *inputContext);
+
+    std::string subMode(const InputMethodEntry &, InputContext &) override;
+
+    KKCState *state(InputContext *ic);
+
+private:
+    void loadDictionary();
+    void loadRule();
+
+    Instance *instance_;
+    FactoryFor<KKCState> factory_;
+    std::unique_ptr<KkcLanguageModel, decltype(&g_object_unref)> model_;
+};
+
+class KKCFactory : public AddonFactory {
+public:
+    AddonInstance *create(AddonManager *manager) override {
+        return new KKC(manager->instance());
+    }
+};
+} // namespace fcitx
+
+#endif // _FCITX_KKC_H_
