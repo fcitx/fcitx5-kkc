9acf24001e00d5f18fccefb98a3ca5caf2234e22
Weng Xuetian <wengxt@gmail.com>
2026-01-24 15:38:55 -0800

    Fix FindLibKKC
    1. pc name is not libkkc, but kkc-1.0
    2. error when pc failed.
    3. fix the link/compile options and cleanup

--- cmake/FindLibKKC.cmake
+++ cmake/FindLibKKC.cmake
@@ -17,38 +17,39 @@
 # use pkg-config to get the directories and then use these values
 # in the find_path() and find_library() calls
 find_package(PkgConfig QUIET)
-pkg_check_modules(PC_LibKKC QUIET libkkc)
+pkg_check_modules(PC_LibKKC QUIET kkc-1.0)
 
 find_path(LIBKKC_INCLUDE_DIR NAMES libkkc/libkkc.h
    HINTS
-   ${PC_LIBKKC_INCLUDEDIR}
-   ${PC_LIBKKC_INCLUDE_DIRS}
+   ${PC_LibKKC_INCLUDEDIR}
+   ${PC_LibKKC_INCLUDE_DIRS}
    )
 
 find_library(LIBKKC_LIBRARIES NAMES kkc
    HINTS
-   ${PC_LIBKKC_LIBDIR}
-   ${PC_LIBKKC_LIBRARY_DIRS}
+   ${PC_LibKKC_LIBDIR}
+   ${PC_LibKKC_LIBRARY_DIRS}
    )
 
-if(PC_LIBKKC_VERSION)
-    set(LIBKKC_VERSION_STRING ${PC_LIBKKC_VERSION})
+if(PC_LibKKC_FOUND)
+    set(LIBKKC_VERSION_STRING ${PC_LibKKC_VERSION})
 endif()
 
 # handle the QUIETLY and REQUIRED arguments and set LIBKKC_FOUND to TRUE if
 # all listed variables are TRUE
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(LibKKC
-                                  REQUIRED_VARS LIBKKC_LIBRARIES LIBKKC_INCLUDE_DIR
+                                  REQUIRED_VARS LIBKKC_VERSION_STRING LIBKKC_LIBRARIES LIBKKC_INCLUDE_DIR
                                   VERSION_VAR LIBKKC_VERSION_STRING)
 
 mark_as_advanced(LIBKKC_INCLUDE_DIR LIBKKC_LIBRARIES)
 
 if (LIBKKC_FOUND AND NOT TARGET LibKKC::LibKKC)
-    add_library(LibKKC::LibKKC UNKNOWN IMPORTED)
+    add_library(LibKKC::LibKKC INTERFACE IMPORTED)
     set_target_properties(LibKKC::LibKKC PROPERTIES
-        IMPORTED_LOCATION "${LIBKKC_LIBRARIES}"
-        INTERFACE_INCLUDE_DIRECTORIES "${LIBKKC_INCLUDE_DIR}"
+        INTERFACE_COMPILE_OPTIONS "${PC_LibKKC_CFLAGS}"
+        INTERFACE_LINK_OPTIONS "${PC_LibKKC_LDFLAGS_OTHER}"
+        INTERFACE_LINK_LIBRARIES "${PC_LibKKC_LINK_LIBRARIES}"
     )
 endif()
 
